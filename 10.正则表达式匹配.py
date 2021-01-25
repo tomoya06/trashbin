91d5e80f7987f11ba4619fa61523adf6f8d4a3c9
pengjiahui.tomoya06 <pengjiahui.tomoya06@bytedance.com>
2021-01-25 21:27:47 +0800

    update: merge fils

--- 10.正则表达式匹配.py
+++ 10.正则表达式匹配.py
@@ -5,6 +5,42 @@
 #
 
 # @lc code=start
+class MySolution:
+    def isMatch(self, s: str, p: str) -> bool:
+        def splitSecs(p) -> List[str]:
+            secs = []
+            i = 0
+            while i < len(p):
+                if i < len(p)-1 and p[i+1] == '*':
+                    secs.append(p[i:i+2])
+                    i += 2
+                else:
+                    secs.append(p[i])
+                    i += 1
+            return secs
+        
+        secs = splitSecs(p)
+
+        def isTotallyMatch(ss: str, pp: List[str]) -> bool:
+            if len(ss) == 0:
+                if len(pp) == 0:
+                    return True
+                if len(pp[0]) == 2:
+                    return isTotallyMatch(ss, pp[1:])
+                return False
+            # len(ss) > 0
+            if len(pp) == 0:
+                return False
+            if len(pp[0]) == 1:
+                return (ss[0] == pp[0] or pp[0] == '.') and isTotallyMatch(ss[1:], pp[1:])
+            # pp[0] has '*'
+            if pp[0] == '.*':
+                return isTotallyMatch(ss[1:], pp) or isTotallyMatch(ss, pp[1:])
+            return ss[0] == pp[0][0] and isTotallyMatch(ss[1:], pp) or \
+                isTotallyMatch(ss, pp[1:])
+        
+        return isTotallyMatch(s, secs)
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         dp = [[False] * (len(s)+1) for _ in range(len(p)+1)]
@@ -24,6 +60,5 @@ class Solution:
         
         return dp[-1][-1]
  
-
 # @lc code=end
 
--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
 New Profile for leetcode hard challenge
 
 > Actually I may copy from so many GULUs solutions :)
+
+## 指路
+
+- 反转链表：30，92，25
+- 
