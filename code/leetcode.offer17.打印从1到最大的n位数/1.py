f09e047dc9221a9b045b9e48e5f8b0fac6cbc408
ITIStomoya06 <35499042+tomoya06@users.noreply.github.com>
2021-08-08 01:45:28 +0800

    update

---
+++ code/leetcode.offer17.打印从1到最大的n位数/1.py
@@ -0,0 +1,15 @@
+# ##剑指offer#数学问题##levelm
+# 工程解法 考虑大数问题
+class Solution:
+    def printNumbers(self, n: int) -> List[int]:
+        realRes = [str(i) for i in range(10)]
+        for level in range(2, n+1):
+            # 处理level位数
+            levelRes = []
+            for top in range(1, 10):
+                # 首位1~9 拼接结果
+                for suffix in realRes:
+                    cur = '{}{}{}'.format(top, '0'*(level-len(suffix)-1), suffix)
+                    levelRes.append(cur)
+            realRes += levelRes
+        return [int(r) for r in realRes[1:]]
---
+++ code/leetcode.offer17.打印从1到最大的n位数/2_偷懒解法.py
@@ -0,0 +1,3 @@
+class Solution:
+    def printNumbers(self, n: int) -> List[int]:
+        return list(range(1, pow(10, n)))
